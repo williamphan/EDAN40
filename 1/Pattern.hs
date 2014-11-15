50f19b41428e18c5cd9d0b33b504a73734137e86
William Phan <william.da.phan@gmail.com>
2014-11-15 20:00:19 +0100

    initial commit

---
+++ 1/Pattern.hs
@@ -0,0 +1,65 @@
+module Pattern where
+import Utilities
+
+
+-------------------------------------------------------
+-- Match and substitute
+--------------------------------------------------------
+
+-- Replaces a wildcard in a list with the list given as the third argument
+substitute :: Eq a => a -> [a] -> [a] -> [a]
+substitute _ [] _ = [] -- Stop when list is empty
+substitute x (y:ys) zs	
+ | x == y	= zs ++ (substitute x ys zs) -- x == y // y == x?
+ | otherwise 	= y: (substitute x ys zs)
+
+
+-- Tries to match two lists. If they match, the result consists of the sublist
+-- bound to the wildcard in the pattern list.
+match :: Eq a => a -> [a] -> [a] -> Maybe [a]
+match _ [] [] = Nothing  -- []?
+match _ [] _ = Nothing
+match _ _ [] = Nothing
+match 
+{- TO BE WRITTEN -}
+
+
+-- Helper function to match
+singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
+singleWildcardMatch (wc:ps) (x:xs) = Nothing
+{- TO BE WRITTEN -}
+longerWildcardMatch (wc:ps) (x:xs) = Nothing
+{- TO BE WRITTEN -}
+
+
+
+-- Test cases --------------------
+
+
+testPattern =  "a=*;"
+testSubstitutions = "32"  -- ska vara 32
+testString = "a=32;"
+
+substituteTest = substitute '*' testPattern testSubstitutions
+substituteCheck = substituteTest == testString
+
+matchTest = match '*' testPattern testString
+matchCheck = matchTest == Just testSubstitutions
+
+
+
+-------------------------------------------------------
+-- Applying patterns
+--------------------------------------------------------
+
+-- Applying a single pattern
+transformationApply :: Eq a => a -> ([a] -> [a]) -> [a] -> ([a], [a]) -> Maybe [a]
+transformationApply _ _ _ _ = Nothing
+{- TO BE WRITTEN -}
+
+
+-- Applying a list of patterns until one succeeds
+transformationsApply :: Eq a => a -> ([a] -> [a]) -> [([a], [a])] -> [a] -> Maybe [a]
+transformationsApply _ _ _ _ = Nothing
+{- TO BE WRITTEN -}
+
